5b23a41ae40e4ac7576820011b7ffc7b8c211f60
yuuki <yuuki@yuukiPC.>
2026-02-17 14:37:20 +0900

    feat/ボタンの追加

---
+++ frontend/main.js
@@ -0,0 +1,15 @@
+$('#btn-back').on('click', function() {
+      console.log("戻るボタンが押されました");
+});
+$('#btn-apply').on('click', function() {
+      console.log("決定ボタンが押されました");
+});
+$('#btn-display').on('click', function() {
+      console.log("予測表示切替ボタンが押されました");
+});
+$('#btn-attack').on('click', function() {
+      console.log("攻撃ボタンが押されました");
+});
+$('#btn-move').on('click', function() {
+      console.log("移動ボタンが押されました");
+});
