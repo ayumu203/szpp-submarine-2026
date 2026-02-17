249a1d1008069bf5e665b301be69557329793703
noyu38 <nojiyu.38@icloud.com>
2026-02-18 00:41:47 +0900

    chore: jsdoc形式のコメントを追加

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,9 @@
 const table = document.querySelector("table#field");
 
+/**
+ * @description jsonファイルからモックデータを取得する
+ * @returns {object} モックデータ
+ */
 async function getMock() {
 
       const response = await fetch("mock/mock.json");
@@ -8,7 +12,34 @@ async function getMock() {
       return data;
 }
 
-// 潜水艦の位置を取得、表示する
+/** @description 表の内容をすべて空にする */
+function clearSubmarines() {
+      for (let row = 1; row <= 5; row++) {
+            for (let col = 1; col <= 5; col++) {
+                  if (table) {
+                        table.rows[row].cells[col].textContent = "";
+                  }
+            }
+      }
+}
+
+/**
+ * @description 潜水艦の位置を●で表示する
+ * @param {number[]} xPos x座標のリスト
+ * @param {number[]} yPos y座標のリスト
+*/
+function renderSubmarines(xPos, yPos) {
+      for (let i = 0; i < xPos.length; i++) {
+            const x = xPos[i];
+            const y = yPos[i];
+            if (x < 1 || x > 5 || y < 1 || y > 5) continue;
+            table.rows[y].cells[x].textContent = "●";
+      }
+}
+
+/**
+ * @description 潜水艦の位置を取得、表示する
+*/
 async function getPos() {
       const data = await getMock();
       const submarines = Object.values(data.State.GetGameStateResponse.allyBoard.submarines);
@@ -24,23 +55,5 @@ async function getPos() {
       renderSubmarines(xPos, yPos);
 }
 
-// 表の内容をすべて空にする
-function clearSubmarines() {
-      for (let row = 1; row <= 5; row++) {
-            for (let col = 1; col <= 5; col++) {
-                  table.rows[row].cells[col].textContent = "";
-            }
-      }
-}
-
-// 潜水艦の位置を●で表示する
-function renderSubmarines(xPos, yPos) {
-      for (let i = 0; i < 4; i++) {
-            x = xPos[i];
-            y = yPos[i];
-            if (x < 1 || x > 5 || y < 1 || y > 5) return;
-            table.rows[y].cells[x].textContent = "●";
-      }
-}
 
 document.addEventListener("DOMContentLoaded", getPos);
