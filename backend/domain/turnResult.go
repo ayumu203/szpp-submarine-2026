5f63aab3143d7356b510c127267889900a5bba58
aaaa1024 <suga.ryota.24@shizuoka.ac.jp>
2026-02-17 16:49:17 +0900

    feat: turnResultの一部変数についてprivateに変更し, Getterを追加

--- backend/domain/turnResult.go
+++ backend/domain/turnResult.go
@@ -7,8 +7,20 @@ import (
 type TurnResult struct {
 	AttackReport share.AttackReportType
 	MoveReport   share.MoveReportType
-	ErrorCode    share.ErrorCode
+	errorCode    share.ErrorCode
 	HitCount     int
-	SunkCount    int
-	NextPlayerId string
+	sunkCount    int
+	nextPlayerId string
+}
+
+func (tr *TurnResult) GetErrorCode() share.ErrorCode {
+	return tr.errorCode
+}
+
+func (tr *TurnResult) GetSunkCount() int {
+	return tr.sunkCount
+}
+
+func (tr *TurnResult) GetNextPlayerId() string {
+	return tr.nextPlayerId
 }
