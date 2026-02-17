400cdac4279343a07de6781b11fc2b701b6aefb0
aaaa1024 <suga.ryota.24@shizuoka.ac.jp>
2026-02-17 16:57:44 +0900

    feat: errorメッセージを追加

--- backend/domain/shared/errors.go
+++ backend/domain/shared/errors.go
@@ -5,5 +5,9 @@ import (
 )
 
 var (
+    ErrInvalidTurn = errors.New("Error[TurnResult.go]: ターンの値が不正です．")
+    ErrInvalidAction = errors.New("Error[Action.go]: アクションが不正です．")
+    ErrInvalidTarget = errors.New("Error[Target.go]: 座標が不正です．")
+    ErrInvalidMoveDistance = errors.New("Error[Move.go]: 移動距離が不正です．")
     ErrOutOfBoard = errors.New("Error[Position.go]: 場所がボードの外です．")
 )
