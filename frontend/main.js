3aac54acbfeb7482915db5b9a0d3301831620aee
noyu38 <nojiyu.38@icloud.com>
2026-02-17 17:42:15 +0900

    feat/潜水艦の位置を表示

--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,43 @@
-$('#btn-back').on('click', function() {
-      console.log("戻るボタンが押されました");
-});
-$('#btn-apply').on('click', function() {
-      console.log("決定ボタンが押されました");
-});
-$('#btn-display').on('click', function() {
-      console.log("予測表示切替ボタンが押されました");
-});
-$('#btn-attack').on('click', function() {
-      console.log("攻撃ボタンが押されました");
-});
-$('#btn-move').on('click', function() {
-      console.log("移動ボタンが押されました");
-});
+const table = document.querySelector("table#field");
+
+async function getMock() {
+
+      const response = await fetch("mock/mock.json");
+      const data = await response.json();
+
+      return data;
+}
+
+async function getPos() {
+      const data = await getMock();
+      const submarines = Object.values(data.State.GetGameStateResponse.allyBoard.submarines);
+      const xPos = [];
+      const yPos = [];
+
+      submarines.forEach(submarine => {
+            xPos.push(submarine.x);
+            yPos.push(submarine.y);
+      });
+
+      clearSubmarines();
+      renderSubmarines(xPos, yPos);
+}
+
+function clearSubmarines() {
+      for (let row = 1; row <= 5; row++) {
+            for (let col = 1; col <= 5; col++) {
+                  table.rows[row].cells[col].textContent = "";
+            }
+      }
+}
+
+function renderSubmarines(xPos, yPos) {
+      for (let i = 0; i < 4; i++) {
+            x = xPos[i];
+            y = yPos[i];
+            if (x < 1 || x > 5 || y < 1 || y > 5) return;
+            table.rows[y].cells[x].textContent = "●";
+      }
+}
+
+document.addEventListener("DOMContentLoaded", getPos);
