bc1e262f8d44fa43d91eeb9d746ce86212c559f9
Ayumu <ochiohita@gmail.com>
2026-02-18 11:15:38 +0900

    feat: Submarineの実装.

---
+++ backend/domain/submarine.go
@@ -0,0 +1,53 @@
+package domain
+
+import (
+	shared "backend/domain/shared"
+)
+
+type Submarine struct {
+	id       string
+	ownerId  string
+	position Position
+	hp       int
+}
+
+func NewSubmarine(id string, ownerId string, position Position, hp int) (*Submarine, error) {
+	if id == "" {
+		return nil, shared.ErrSubmarineIdIsEmpty
+	}
+	if ownerId == "" {
+		return nil, shared.ErrOwnerIdIsEmpty
+	}
+	isWithin, err := position.withinBoard()
+	if err != nil {
+		return nil, err
+	}
+	if !isWithin {
+		return nil, shared.ErrOutOfBoard
+	}
+	if hp < 0 {
+		return nil, shared.ErrInvalidHp
+	}
+	return &Submarine{
+		id:       id,
+		ownerId:  ownerId,
+		position: position,
+		hp:       hp,
+	}, nil
+}
+
+func (submarine *Submarine) GetId() string {
+	return submarine.id
+}
+
+func (submarine *Submarine) GetOwnerId() string {
+	return submarine.ownerId
+}
+
+func (submarine *Submarine) GetPosition() Position {
+	return submarine.position
+}
+
+func (submarine *Submarine) GetHp() int {
+	return submarine.hp
+}
